cb89b88024d9c955b0aaf3358f978c00484104e4
Amit Chaudhari <achaudhari@guidewire.com>
2019-03-03 20:54:12 -0800

    add not implemented page

--- pet-clinic-data/src/main/java/it/wojtas/petclinic/service/map/VetServiceMap.java
+++ pet-clinic-data/src/main/java/it/wojtas/petclinic/service/map/VetServiceMap.java
@@ -1,7 +1,8 @@
 package it.wojtas.petclinic.service.map;
 
+import it.wojtas.petclinic.model.Speciality;
 import it.wojtas.petclinic.model.Vet;
-import it.wojtas.petclinic.service.CrudService;
+import it.wojtas.petclinic.service.SpecialityService;
 import it.wojtas.petclinic.service.VetService;
 import org.springframework.stereotype.Service;
 
@@ -9,6 +10,12 @@ import java.util.Set;
 
 @Service
 public class VetServiceMap extends AbstractMapService<Vet, Long> implements VetService {
+    private final SpecialityService specialityService;
+
+    public VetServiceMap(SpecialityService specialityService) {
+        this.specialityService = specialityService;
+    }
+
     @Override
     public Vet findById(Long id) {
         return super.findById(id);
@@ -16,6 +23,14 @@ public class VetServiceMap extends AbstractMapService<Vet, Long> implements VetS
 
     @Override
     public Vet save(Vet owner) {
+        if (owner.getSpecialities().size() > 0) {
+            owner.getSpecialities().forEach(spec -> {
+                if (spec.getId() == null) {
+                    Speciality savedSpec = specialityService.save(spec);
+                    spec.setId(savedSpec.getId());
+                }
+            });
+        }
         return super.save(owner);
     }
 
